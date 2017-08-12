153fdd54b55f90e4b3a21a8a2a54f920c918d923
maverick98 <abeautifulmind98@gmail.com>
2017-08-12 20:46:49 +0530

    smooth curve yet remains  a dream

--- j-hawk-code/plot/plot/plotter.py
+++ j-hawk-code/plot/plot/plotter.py
@@ -1,21 +1,45 @@
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
 import numpy as np
+import dateutil.parser
+
+def date_linspace(start, end, steps):
+  delta = (end - start) / steps
+  increments = range(0, steps) * np.array([delta]*steps)
+  return start + increments
+
 def plotSmoothCurve(file,title,xlabel,ylabel, T , power ):
-    xnew = np.linspace(T.min(), T.max(), 300)
-    power_smooth = spline(T, power, xnew)
+    xnew = date_linspace(T.min(), T.max(),4)
+
+    #power_smooth = spline(T, power, xnew)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.plot(xnew, power_smooth)
+    plt.plot(xnew, power)
     plt.savefig(file, dpi=100)
     plt.show()
     return
 
-T = np.array([6, 7, 8, 9, 10, 11, 12])
-power = np.array([1.53E+03, 5.92E+02, 2.04E+02, 7.24E+01, 2.72E+01, 1.10E+01, 4.70E+00])
 
-plotSmoothCurve("test.png","myTitle","Time","Time Taken in seconds", T, power)
+
+T1 = np.array([])
+power1 = np.array([])
+lines = [line.rstrip('\n') for line in open('perf.log')]
+for line in lines:
+    page,startTime,endTime,duration = line.split(",")
+
+    yourdate = dateutil.parser.parse(startTime)
+
+    T1 = np.append(T1,yourdate)
+
+    power1=np.append(power1,duration)
+
+
+
+
+
+
+plotSmoothCurve("test.png","myTitle","Time","Time Taken in seconds", T1, power1)
 
 
 

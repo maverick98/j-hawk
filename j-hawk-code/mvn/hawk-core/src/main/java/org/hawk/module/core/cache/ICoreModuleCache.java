a8b76c0ddb4ffdb177382e2e945a538f3e2ee798
Mano <manoranjan.sahu@oracle.com>
2024-08-23 22:34:24 +0530

    Fixed

--- j-hawk-code/mvn/hawk-core/src/main/java/org/hawk/module/core/cache/ICoreModuleCache.java
+++ j-hawk-code/mvn/hawk-core/src/main/java/org/hawk/module/core/cache/ICoreModuleCache.java
@@ -1,6 +1,6 @@
 /**
   * This file is part of impensa.
-  * CopyLeft (C) BigBang<->BigCrunch.All Rights are left.
+  *  
   *
   * 1) Modify it if you can understand.
   * 2) If you distribute a modified version, you must do it at your own risk.
